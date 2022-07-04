81bc8216c794657dd0b3c68145d3ab6e14792efd
Saidi <saidi.ahamada@gmail.com>
2022-07-04 23:48:59 +0200

    fixed menu on scroll

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const nav = document.querySelector('.header-nav');
+window.addEventListener("scroll", (event) => {
+    window.scrollY > nav.offsetTop ? nav.classList.add('fixed') : nav.classList.remove('fixed');
+});fixed
